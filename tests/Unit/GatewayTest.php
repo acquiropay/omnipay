1260a09f60f032c97a4d5bbdd12e9dc71bea83f9
Vladimir Yuldashev <misterio92@gmail.com>
2017-06-07 18:33:54 +0300

    100% coverage

--- tests/Unit/GatewayTest.php
+++ tests/Unit/GatewayTest.php
@@ -21,15 +21,30 @@ class GatewayTest extends GatewayTestCase
     public function testAuthorize()
     {
         $options = array(
-            'amount'        => '10.00',
-            'card'          => new CreditCard($this->getValidCard()),
+            'amount' => '10.00',
+            'card' => new CreditCard($this->getValidCard()),
             'transactionId' => uniqid('test_', true),
-            'clientIp'      => '127.0.0.1',
+            'clientIp' => '127.0.0.1',
         );
 
         $request = $this->gateway->authorize($options);
 
         $this->assertInstanceOf('Omnipay\AcquiroPay\Message\AuthorizeRequest', $request);
         $this->assertSame($options['amount'], $request->getAmount());
+    }
+
+    public function testStatusParameters()
+    {
+        foreach ($this->gateway->getDefaultParameters() as $key => $default) {
+            // set property on gateway
+            $getter = 'get' . ucfirst($this->camelCase($key));
+            $setter = 'set' . ucfirst($this->camelCase($key));
+            $value = uniqid();
+            $this->gateway->$setter($value);
+
+            // request should have matching property, with correct value
+            $request = $this->gateway->status();
+            $this->assertSame($value, $request->$getter());
+        }
     }
 }
